8606b523f8c7a030a4e12fcfb14d6e7282a306c9
saranhive <saranyanedumaran888@gmail.com>
2022-05-13 15:49:11 +0530

    first commit

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,54 +1,67 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { initializeApp } from '@firebase/app';
 import { registerModel } from './register.model';
 import { ApiService } from './shared/api.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   formValue !: FormGroup;
-  registerData ! : any;
-  registermodelobj : registerModel = new registerModel();
+  registerData !: any;
+  registermodelobj: registerModel = new registerModel();
 
 
-  constructor(private formbuilder:  FormBuilder,
-    private api :ApiService){
+  constructor(private formbuilder: FormBuilder,
+    private api: ApiService) {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyAZ1g2v2-YaGIP62A_hyESRx-SSDuPE7Sc",
+      authDomain: "fir-loger-bd900.firebaseapp.com",
+      projectId: "fir-loger-bd900",
+      storageBucket: "fir-loger-bd900.appspot.com",
+      messagingSenderId: "792303288054",
+      appId: "1:792303288054:web:7a767b64edadcf9cedf6d7"
+    };
 
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
   }
   ngOnInit(): void {
     this.formValue = this.formbuilder.group({
-      Name : [''],
-      EmailAddress : [''],
-      Password : ['']
+      Name: [''],
+      EmailAddress: [''],
+      Password: ['']
 
     })
     this.getallusers();
   }
-  postregisterdetails(){
+  postregisterdetails() {
     this.registermodelobj.Name = this.formValue.value.Name;
     this.registermodelobj.EmailAddress = this.formValue.value.EmailAddress;
     this.registermodelobj.Password = this.formValue.value.Password;
 
     this.api.postregister(this.registermodelobj)
-    .subscribe(res=>{
-      console.log(res);
-      alert("Registerd Successfully")
-      let ref = document.getElementById('cancel')
-      ref?.click();
-      this.formValue.reset();
-      this.getallusers();
-    },
-    err=>{
-      alert("check");
-    })
+      .subscribe(res => {
+        console.log(res);
+        alert("Registerd Successfully")
+        let ref = document.getElementById('cancel')
+        ref?.click();
+        this.formValue.reset();
+        this.getallusers();
+      },
+        err => {
+          alert("check");
+        })
   }
-    getallusers(){
-      this.api.getregister()
-      .subscribe(res=>{
+  getallusers() {
+    this.api.getregister()
+      .subscribe(res => {
         this.registerData = res;
       })
-    }
-    }
+  }
+}
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,32 +1,36 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { ReactiveFormsModule ,FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+// import { AngularFireModule } from '@angular/fire/compat';
+// import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { initializeApp } from "firebase/app";
 
 //import firebase module
-import { AngularFireModule } from '@angular/fire';
-import{ AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   declarations: [
     AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
+
     ReactiveFormsModule,
     HttpClientModule,
-     AngularFireModule.intializeApp(environment.firebase),
-     AngularFireDatabaseModule,
-     AngularFirestoreModule
+    //  AngularFireModule.initializeApp(environment.firebase),
+    //  AngularFireDatabaseModule,
+    //  AngularFirestoreModule
     
   ],
   providers: [],
